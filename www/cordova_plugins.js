301a1f10bd10011fc913ed3992e9086668924355
Daryl Factor <darylfactor@ymail.com>
2015-09-28 21:54:07 -0700

    added source=npm to config file plugins

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -1,4 +1,3 @@
-/*
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
@@ -30,4 +29,4 @@ module.exports.metadata =
     "org.apache.cordova.dialogs": "0.3.0"
 }
 // BOTTOM OF METADATA
-});*/
+});
